ca85d4a50791a297989d8d085729e4186dddf9d8
eeshaaa <69174015+wonderstruckz@users.noreply.github.com>
2020-08-28 16:46:41 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# class-25
+# TopplingBoxes
+Toppling Boxes
---
+++ pig.js
@@ -0,0 +1,7 @@
+class Pig extends Parent {
+    constructor(x, y) {
+      super(x,y,50,50)
+      this.image = loadImage("sprites/enemy.png")
+    }
+  };
+  
